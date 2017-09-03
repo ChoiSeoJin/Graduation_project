536ba41242e8ae6e78fe100dc9d489f0c4a03eef
ChoiSeoJin <scboys18@gmail.com>
2017-09-03 18:31:43 +0900

    first commit

---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,18 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<!DOCTYPE html>
+<html>
+
+   <head>
+   		<link rel="stylesheet" href = "../css/nav.css">
+      <meta charset="UTF-8">
+      <title>React App</title>
+
+   </head>
+
+   <body>
+      <div id="root"></div>
+      <script src="/built/bundle.js"></script>
+   </body>
+
+</html>
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    entry: './src/main/webapp/js/index.js',
+
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        inline: true,
+        port: 7777,
+        contentBase: __dirname + '/public/'
+    },
+
+    module: {
+            loaders: [
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    loader: 'url-loader?limit=8192'
+                },
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    query: {
+                        cacheDirectory: true,
+                        presets: ['es2015', 'react']
+                    }
+                },
+                {
+                    test : /\.css$/,
+                    loader: 'style-loader!css-loader'
+                }
+            ]
+        }
+};
